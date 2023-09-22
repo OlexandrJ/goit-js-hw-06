9b3201e8becf98917662e0753222e2403ea8de19
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-09-22 12:40:24 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+
+  const formData = new FormData(this);
+  const userData = {};
+
+  formData.forEach((value, key) => {
+    userData[key] = value;
+  });
+
+  if (!userData.email || !userData.password) {
+    alert('All fields must be filled in.');
+    return;
+  }
+
+  console.log(userData);
+  this.reset();
+});
