c97098bbcfaf50ed9a3c53fd415d41182eecdd43
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-09-22 12:57:47 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,39 @@
+const input = document.querySelector('input');
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const boxesDiv = document.querySelector('#boxes');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const initialSize = 30;
+  const step = 10;
+  const boxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${initialSize + i * step}px`;
+    box.style.height = `${initialSize + i * step}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+
+  boxesDiv.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
+
+createButton.addEventListener('click', () => {
+  const amount = input.value;
+  createBoxes(amount);
+});
+
+destroyButton.addEventListener('click', () => {
+  destroyBoxes();
+});
