7049ba67d85206e1bdff19661c26bc503eb13773
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-09-23 11:27:27 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,19 @@
-const categoriesList = document.querySelectorAll('ul#categories > li.item');
+// const categoriesList = document.querySelectorAll('ul#categories > li.item');
+// console.log(`Number of categories: ${categoriesList.length}`);
+
+// categoriesList.forEach(category => {
+//     const categoryName = category.querySelector('h2').textContent;
+//     const categoryElements = category.querySelectorAll('ul > li');
+//   console.log(`Category: ${categoryName}`);
+//   console.log(`Elements: ${categoryElements.length}`);
+// });
+
+const categoriesList = document.querySelectorAll('#categories .item');
 console.log(`Number of categories: ${categoriesList.length}`);
 
 categoriesList.forEach(category => {
-    const categoryName = category.querySelector('h2').textContent;
-    const categoryElements = category.querySelectorAll('ul > li');
-  console.log(`Category: ${categoryName}`);
+  const categoryTitle = category.firstElementChild.textContent;
+  const categoryElements = category.lastElementChild.children;
+  console.log(`Category: ${categoryTitle}`);
   console.log(`Elements: ${categoryElements.length}`);
 });
